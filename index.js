e2269aab9d77c1586e67ffb4abecd2d40286a8fe
Priyansh Singh <singhpriyansh2000@gmail.com>
2022-03-08 02:31:35 +0530

    called the jsonplaceholder api for placeholder blog posts

---
+++ index.js
@@ -0,0 +1,13 @@
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((res) => res.json())
+  .then((data) => {
+    const postsArr = data.slice(0, 5);
+    let html = " ";
+    for (post of postsArr)
+      html += `
+    <h3>${post.title}</h3>
+    <p>${post.body}</p>
+    <hr />
+    `;
+    document.getElementById("blog-list").innerHTML = html;
+  });
