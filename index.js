5cbed7284d92b02f286c53eca8652e5a01648d43
Priyansh Singh <singhpriyansh2000@gmail.com>
2024-10-13 23:55:46 +0530

    fix styling of the blog

--- index.js
+++ index.js
@@ -4,46 +4,45 @@ const bodyInput = document.getElementById("post-body");
 const form = document.getElementById("new-post");
 
 function renderPosts() {
-  let html = " ";
-  for (post of postsArray)
-    html += `
-    <h3>${post.title}</h3>
-    <p>${post.body}</p>
-    <hr />
-    `;
-  document.getElementById("blog-list").innerHTML = html;
+    let html = "";
+    for (let i = 0; i < postsArray.length; i++) {
+        html += `
+        <div class="blog-post">
+            <h3>${postsArray[i].title}</h3>
+            <p>${postsArray[i].body}</p>
+            <button class="delete-btn" onclick="deletePost(${i})">Delete</button>
+        </div>
+        `;
+    }
+    document.getElementById("blog-list").innerHTML = html;
 }
 
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((res) => res.json())
-  .then((data) => {
-    postsArray = data.slice(0, 10);
+function deletePost(index) {
+    postsArray.splice(index, 1);
     renderPosts();
-  });
+}
+
 
 form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const postTitle = titleInput.value;
-  const postBody = bodyInput.value;
-  const data = {
-    title: postTitle,
-    body: postBody,
-  };
+    e.preventDefault();
+    const postTitle = titleInput.value;
+    const postBody = bodyInput.value;
+    const data = {
+        title: postTitle,
+        body: postBody,
+    };
 
-  let options = {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-
-  fetch("https://jsonplaceholder.typicode.com/posts", options)
-    .then((res) => res.json())
-    .then((post) => {
-      postsArray.unshift(post);
-      console.log(postsArray);
-      renderPosts();
-      form.reset();
-    });
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((res) => res.json())
+        .then((post) => {
+            postsArray.unshift(post);
+            renderPosts();
+            form.reset();
+        });
 });
